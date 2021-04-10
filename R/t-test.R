b60eebe7e64fb3920965caf3b21cdf3d508c4fa9
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-04-10 22:17:57 +0200

    Add a simple R file and documentation.

---
+++ R/t-test.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param x
+#' @param y
+#' @param alternative
+#' @param mu
+#' @param paired
+#' @param var.equal
+#' @param conf.level
+#'
+#' @return
+#' @export
+#'
+#' @examples
+t_test <- function(x, y = NULL,
+                   alternative = c("two_tail", "left_tail", "right_tail"),
+                   mu = 0, paired = FALSE, var.equal = FALSE,
+                   conf.level = 0.95) {
+
+}
